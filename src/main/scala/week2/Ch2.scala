fab9931e68f48edde37eac35de0d288dd382fb50
Devon Stewart <blast@hardchee.se>
2015-10-05 14:07:21 -0700

    Ex2.1

--- src/main/scala/week2/Ch2.scala
+++ src/main/scala/week2/Ch2.scala
@@ -1,6 +1,22 @@
 package week2
 
 object Ch2 {
+  def fib(n: Int): Int = {
+    val initial = (0, 1)
+    val f: (Int, Int) => Int = _ + _
+
+    val (fst, _) = (0 until n).foldLeft(initial) {
+      case (x, _) => x.swap.copy(_2 = f.tupled(x))
+    }
+
+    fst
+  }
+
   def main(args: Array[String]): Unit = {
+    println(s"fib(1): ${fib(1)}")
+    println(s"fib(2): ${fib(2)}")
+    println(s"fib(3): ${fib(3)}")
+    println(s"fib(4): ${fib(4)}")
+    println(s"fib(5): ${fib(5)}")
   }
 }
--- src/test/scala/Ch2Tests.scala
+++ src/test/scala/Ch2Tests.scala
@@ -5,4 +5,10 @@ import org.scalacheck.Prop._
 import org.scalacheck._
 
 object Ch2Specification extends Properties("Ch2") {
+  property("fib") = {
+    def smallInt: Gen[Int] = Gen.choose(2, 100)
+    forAll(smallInt) { n: Int =>
+      Ch2.fib(n - 2) + Ch2.fib(n - 1) == Ch2.fib(n)
+    }
+  }
 }
